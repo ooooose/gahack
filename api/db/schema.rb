077b4e231706cd2cb4fb4ed9c4815cffc17b80a2
ooooose <yuuki.oose1990@gmail.com>
2022-11-19 07:15:03 +0900

    Fix:Userテーブルの修正 #8

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,14 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_18_014833) do
+ActiveRecord::Schema.define(version: 2022_11_18_124810) do
 
-  create_table "users", id: { type: :string, limit: 36 }, charset: "utf8mb4", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
-    t.text "profile"
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change", default: false
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "name"
+    t.string "nickname"
+    t.string "image"
+    t.string "email"
+    t.text "tokens"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
 end
