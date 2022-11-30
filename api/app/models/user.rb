1449ef1ecbd764a751bc91eaacfbdf5c24ea3390
ooooose <yuuki.oose1990@gmail.com>
2022-11-30 21:11:01 +0900

    Fix:modelのアソシエーションを整理

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
+  has_many :pictures
 
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, uniqueness: { case_sensitive: true }
