169c4e26631383ecd28d3e06959a1ef40ad0aacf
ooooose <yuuki.oose1990@gmail.com>
2022-11-30 16:56:51 +0900

    Add:お絵描き機能周りのボタンコンポーネントを作成#9

---
+++ front/src/components/atoms/buttons/UploadButton.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  submitBtn: {
+    marginTop: theme.spacing(2),
+    flexGrow: 1,
+    textTransform: "none"
+  },
+}));
+
+export const UploadButton = (props) => {
+  const classes = useStyles();
+  
+  return (
+    <>
+      <Button
+        variant="contained"
+        size="large"
+        color="default"
+        // disabled={!props.email || !props.password ? true : false}
+        className={classes.submitBtn}
+        onClick={props.uploadCanvas}
+      >
+        {props.children}
+      </Button>
+    </>
+  )
+};
