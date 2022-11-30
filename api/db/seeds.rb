3a8ed6a4e73d3dbdb8e68fc79bc5175ff156d6d3
ooooose <yuuki.oose1990@gmail.com>
2022-11-30 17:13:27 +0900

    Add:テーマのダミーデータを３つ用意#19

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+## 確認用にデータをいくつか用意する。
+
+## テーマを３つほど設定する。
+Theme.create(title: "ドラえもん")
+Theme.create(title: "のび太")
+Theme.create(title: "とんかつDJ")
