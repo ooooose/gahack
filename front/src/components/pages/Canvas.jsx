1138f1c7310eec5120c877fd91312f3b9eb8c694
ooooose <yuuki.oose1990@gmail.com>
2022-11-30 16:58:13 +0900

    Add:ボタンコンポーネントを反映（一部未実装）#9

--- front/src/components/pages/Canvas.jsx
+++ front/src/components/pages/Canvas.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
+// import { createPicture } from "../../lib/api/pictures";
+import { ResetButton } from "../atoms/buttons/ResetButton";
+// import { UploadButton } from "../atoms/buttons/UploadButton";
 
 const Canvas = () => {
   const [drawFlag, setDrawFlag] = useState(0);
+  const [drawJudgement, setDrawJudgement] = useState(0);
+  const bgColor = 'rgb(255,255,255)';
   let canvas;
   let ctx;
   let Xpoint, Ypoint;
@@ -34,6 +39,7 @@ const Canvas = () => {
       Xpoint = e.clientX - rect.left;
       Ypoint = e.clientY - rect.top;
       setDrawFlag(1);
+      setDrawJudgement(1);
       ctx.lineTo(Xpoint, Ypoint);
       ctx.lineCap = "round";
       ctx.strokeStyle = x;
@@ -79,7 +85,49 @@ const Canvas = () => {
     }
   }
 
+  const resetCanvas = () => {
+    if (window.confirm('本当にリセットしますか？')) {
+      setDrawJudgement(0);
+      ctx.fillStyle = bgColor;
+      ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+    }
+  }
 
+  // 描いた絵をアップロードする機能を実装（無限レンダリングされてるっぽいので一旦コメントアウト）
+  // const createPicture = () => {
+  //   if (drawJudgement === 0) {
+  //     window.alert('何か記入してください');
+  //   } else {
+  //     canvas.toBlob((blob) => {
+  //       let reader = new FileReader();
+  //       // CSRFトークンを発行（おそらくここの取得はできていない。一旦コメントアウト）
+  //       // const token = document.getElementsByName("csrf-token")[0].content;
+  //       reader.readAsDataURL(blob);
+
+  //       reader.onload = async (e) => {
+  //         e.preventDefault();
+  //         let dataUrlBase64 = reader.result;
+  //         let base64 = dataUrlBase64.replace(/data:.*\/.*;base64,/, '');
+  //         const data = {
+  //           image: base64
+  //         }
+  //         // エラー回避のメソッドを実行。適切に実行されるかを確認する。
+  //         try {
+  //           // 確認用コンソール出力です。実装できたら消してください。
+  //           console.log("ここだ！");
+  //           const res = await createPicture(data);
+  //           if (res.status === 200) {
+  //             // ページ遷移するようにする
+  //             console.log("登録されてるかもね。");
+  //           }
+  //         } catch (e) {
+  //           console.log(e);
+  //         }
+  //       };
+  //     // pngファイルで保存するよう第二引数に設定することができる。 
+  //     }, 'image/png');
+  //   };
+  // };
   
   
   const style = {
@@ -95,6 +143,8 @@ const Canvas = () => {
   return (
     <>
       <canvas id="canvas" width="700" height="500" style={style}></canvas>
+      <ResetButton resetCanvas={resetCanvas}>キャンバスをリセットする</ResetButton>
+      {/* <UploadButton uploadCanvas={createPicture}>画像をアップロード</UploadButton> */}
     </>
   )
 }
