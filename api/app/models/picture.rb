cf9fca6ec69c510079a6b9d637595dc6fa6c19a9
ooooose <yuuki.oose1990@gmail.com>
2022-12-06 09:27:07 +0900

    Add:userとpictureのモデルにいいねのアソシエーションを設定#25

--- api/app/models/picture.rb
+++ api/app/models/picture.rb
@@ -1,6 +1,8 @@
 class Picture < ApplicationRecord
   belongs_to :user
   belongs_to :theme
+  has_many :likes
+  has_many :liked_user, through: :likes, source: :user
 
   validates :image, presence: true
   validates :user_id, presence: true
--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
   has_many :pictures
+  has_many :likes
+  has_many :liked_pictures, through: :likes, source: :picture
 
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, uniqueness: { case_sensitive: true }
