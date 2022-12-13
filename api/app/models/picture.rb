0c7ae18d60920c0d611de6e9c4febf784352fb18
ooooose <yuuki.oose1990@gmail.com>
2022-12-13 16:20:49 +0900

    Fix:pictures関連をいいねモデルに紐付け#25

--- api/app/models/picture.rb
+++ api/app/models/picture.rb
@@ -1,10 +1,15 @@
 class Picture < ApplicationRecord
   belongs_to :user
   belongs_to :theme
+  # いいね機能
   has_many :likes
-  has_many :liked_user, through: :likes, source: :user
+  has_many :liked_users, through: :likes, source: :user
 
   validates :image, presence: true
   validates :user_id, presence: true
   validates :theme_id, presence: true
+
+  def liked_by(user)
+    likes.find{ |f| f.user_id == user.id }
+  end
 end
