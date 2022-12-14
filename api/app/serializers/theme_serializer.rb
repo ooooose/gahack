442453b2f742d604bd4b3cfe71577cd28cfb9074
ooooose <yuuki.oose1990@gmail.com>
2022-12-14 20:31:41 +0900

    Fix:ThemeのSerializerに初期値を設定

--- api/app/serializers/theme_serializer.rb
+++ api/app/serializers/theme_serializer.rb
@@ -1,5 +1,10 @@
 class ThemeSerializer < ActiveModel::Serializer
   attributes :id, :title, :created_at
-
   has_many :pictures
+
+  def initialize(object, **option)
+    @current_api_v1_user = option[:current_api_v1_user]
+    super
+  end
+
 end
