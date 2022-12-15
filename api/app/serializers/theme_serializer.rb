143d3718c0427f79a4157e6e83c589ad2b6dd672
ooooose <yuuki.oose1990@gmail.com>
2022-12-15 09:32:40 +0900

    Fix:themeに関するSerializerの更新#19

--- api/app/serializers/theme_serializer.rb
+++ api/app/serializers/theme_serializer.rb
@@ -1,5 +1,5 @@
 class ThemeSerializer < ActiveModel::Serializer
-  attributes :id, :title, :created_at
+  attributes %i[id title created_at]
   has_many :pictures
 
   def initialize(object, **option)
