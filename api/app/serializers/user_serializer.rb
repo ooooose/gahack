113dbcf6a1d3c32007040f977e0139148af7882d
ooooose <yuuki.oose1990@gmail.com>
2022-12-15 09:31:21 +0900

    Add:usersコントローラにshowアクションを追加#20

--- api/app/serializers/user_serializer.rb
+++ api/app/serializers/user_serializer.rb
@@ -1,7 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name
+
   def initialize(object, **option)
     @current_api_v1_user = option[:current_api_v1_user]
     super
+  end
+
+  attribute :pictures do
+    object.pictures
   end
 end
