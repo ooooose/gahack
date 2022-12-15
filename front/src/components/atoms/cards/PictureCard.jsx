d4b8c9b39883350a9af278e4d5020081ccecad03
ooooose <yuuki.oose1990@gmail.com>
2022-12-15 10:06:24 +0900

    Add:PictureCardコンポーネントにLikesコンポーネントを埋め込み#25

--- front/src/components/atoms/cards/PictureCard.jsx
+++ front/src/components/atoms/cards/PictureCard.jsx
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Picture from "../picture/Picture";
 import { Link } from "react-router-dom";
 
-import LikeButton from "../buttons/LikeButton";
-import UnLikeButton from "../buttons/UnlikeButton";
+import Likes from "../../molecules/Likes";
 
 import Card from "@material-ui/core/Card";
 import { makeStyles } from "@material-ui/core/styles";
@@ -18,17 +17,8 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 const PictureCard = ({picture, pictureId }) => {
-  const [likes, setLikes] = useState(picture.likes);
-  const [likeState, setLikeState] = useState(picture.liked);
   const classes = useStyles();
 
-  const generateParams = () => {
-    const likeParams = {
-      picture_id: pictureId,
-    };
-    return likeParams;
-  };
-  
   return (
     <>
       <Card
@@ -46,24 +36,7 @@ const PictureCard = ({picture, pictureId }) => {
             image={picture.image}
             />          
         </Link>
-        { likeState ? (
-          <UnLikeButton 
-            params={generateParams()} 
-            likeState={picture.liked}
-            setLikeState={setLikeState}
-            likeId={picture.like_id}
-            likes={likes}
-            setLikes={setLikes}
-          />
-        ) : (
-          <LikeButton 
-            params={generateParams()} 
-            likeState={picture.liked}
-            setLikeState={setLikeState}
-            likes={likes}
-            setLikes={setLikes}
-          />
-        )}
+        <Likes picture={picture} pictureId={pictureId} />
       </Card>
     </>
   )
