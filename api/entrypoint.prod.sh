a423a2242225479d58b31b66e7cfac4516dc01c5
ooooose <yuuki.oose1990@gmail.com>
2023-01-04 22:51:20 +0900

    [Fix]:本番環境用にapi側Dockerfileを編集

--- api/entrypoint.prod.sh
+++ api/entrypoint.prod.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -e
 
-# 下３行はpushするときに有効にする。
-# bundle exec rails db:create RAILS_ENV=production
-# bundle exec rails db:migrate RAILS_ENV=production
-# bundle exec rails db:seed RAILS_ENV=production
-
 rm -f /myapp/tmp/pids/server.pid
 
-exec "$@"
+# bin/setup
+bundle exec pumactl start
