6003b280cf3bca93cb598ab01da7fc3e73bd476a
ooooose <yuuki.oose1990@gmail.com>
2023-01-22 23:59:56 +0900

    [Add]:commentのAPI通信用にコンポーネントを作成

---
+++ front/src/lib/api/comments.js
@@ -0,0 +1,19 @@
+import client from "./client";
+import Cookies from "js-cookie";
+
+export const createComment = (params, pictureId) => {
+  return client.post(`pictures/${pictureId}/comments`, params, { headers: {
+    "access-token": Cookies.get("_access_token"),
+    "client": Cookies.get("_client"),
+    "uid": Cookies.get("_uid"),
+  }});
+};
+
+export const deleteComment = (pictureId, id) => {
+  return client.delete(`/pictures/${pictureId}/comments/${id}`,
+  { headers: {
+    "access-token": Cookies.get("_access_token"),
+    "client": Cookies.get("_client"),
+    "uid": Cookies.get("_uid"),
+  }})
+}
