975d6be7c0be415a197ff549831ed2895ffb7338
ooooose <yuuki.oose1990@gmail.com>
2023-02-04 07:19:51 +0900

    [Add]フォロー・フォロー解除ボタンの実装

---
+++ front/src/components/atoms/buttons/UnFollowButton.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { makeStyles, Button } from "@material-ui/core";
+
+import { destroyRelationship } from '../../../lib/api/relationships';
+
+const useStyles = makeStyles((theme) => ({
+  submitBtn: {
+    marginTop: theme.spacing(2),
+    flexGrow: 1,
+    textTransform: "none",
+    color: 'black',
+  },
+}));
+
+const UnFollowButton = ({ userId, params, setFollowState }) => {
+  const classes = useStyles();
+  const handleDeleteRelationship = async () => {
+    try {
+      const res = await destroyRelationship(userId, params);
+      if (res.status === 200) {
+        setFollowState(false);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return (
+    <>
+      <Button
+        variant="outlined"
+        color="secondary"
+        onClick={handleDeleteRelationship}
+        className={classes.submitBtn}
+      >
+        フォロー解除
+      </Button>
+    </>
+  )
+};
+
+export default UnFollowButton;
