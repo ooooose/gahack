e4e3460da8b9fbefc15d8fbaa5832a1f64a3c9c6
ooooose <yuuki.oose1990@gmail.com>
2023-02-06 22:32:47 +0900

    [Add]twitter_cardカラムにcarrierwaveのuploaderをマウント

--- api/app/models/picture.rb
+++ api/app/models/picture.rb
@@ -5,6 +5,7 @@ class Picture < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   has_many :comments, dependent: :destroy
+  mount_uploader :twitter_card, ImageUploader
 
   # 描かれた絵を作成順に並び替える
   scope :by_recently_created, -> { order(created_at: :desc) }
@@ -13,8 +14,8 @@ class Picture < ApplicationRecord
   validates :user_id, presence: true
   validates :theme_id, presence: true
 
-
   def liked_by(user)
     likes.find{ |f| f.user_id == user.id }
   end
+
 end
