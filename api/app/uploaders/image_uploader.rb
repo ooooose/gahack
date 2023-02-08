9c273452eaa8176ed3c39d3ca2f20370e9e691da
ooooose <yuuki.oose1990@gmail.com>
2023-02-08 19:38:03 +0900

    [Add]Image_uploaderにmini_magickの処理を追記

--- api/app/uploaders/image_uploader.rb
+++ api/app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -49,4 +49,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   def filename
     "#{SecureRandom.uuid}.#{file.extension}" if original_filename
   end
+
+  process resize_to_limit: [200, 200]
 end
