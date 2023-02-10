308b2a1cef77b129f7381b90de8a8aa8d497f6e9
ooooose <yuuki.oose1990@gmail.com>
2023-02-10 16:49:09 +0900

    [Fix]遷移先など軽微な修正

--- front/src/components/pages/NotFound.jsx
+++ front/src/components/pages/NotFound.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import { Button, makeStyles } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: '100px',
+  },
+  notFound: {
+    fontSize: '50px',
+  },
+  link: {
+    color: 'white',
+    textDecoration: 'none',
+  }
+}));
 
 const NotFound = () => {
+  const classes = useStyles();
   return(
     <>
-      <h1>404</h1>
-      <h3>お探しのページは見つかりませんでした。</h3>
+      <div>
+        <h1 className={classes.notFound}>404</h1>
+        <h3>お探しのページは見つかりませんでした。</h3>
+        <Button color="primary" variant="contained" >
+          <Link to="/" className={classes.link} >
+            ホームに戻る
+          </Link>
+        </Button>
+      </div>
     </>
   )
 }
