6c214773aaea3c46bd6acb5e95e322c201b8ead2
ooooose <yuuki.oose1990@gmail.com>
2023-02-12 14:10:11 +0900

    [Fix]TOPページの文言やスタイル修正

--- front/src/components/organisms/TopDescription.jsx
+++ front/src/components/organisms/TopDescription.jsx
@@ -23,7 +23,7 @@ const useStyles = makeStyles((theme) => ({
   },
   rightCard: {
     padding: theme.spacing(2),
-    maxWidth: 250,
+    maxWidth: 300,
     backgroundColor: 'rgba(190,184,245,0.5)',
     borderRadius: '18px',
     margin: '0 auto 5px',
@@ -38,14 +38,14 @@ const TopDescription = () => {
         <Grid className={`${styles.cardWrapper}`} container>
           <Grid item xs={12}>
             <div>
-              <Typography variant="h5">サービスについて</Typography>
+              <Typography variant="h5" style={{ fontWeight: 'bold' }} >サービスについて</Typography>
               <p className={`${styles.description}`}>お題に沿った絵を描き、投稿できるサービスです。<br/> 童心に返って、自由に絵を描きましょう！！</p>
             </div>
           </Grid>
         </Grid>
         <Grid className={`${styles.cardWrapper}`} container>
           <Grid item xs={12}>
-            <Typography variant="h5">サービスの使い方</Typography>          
+            <Typography variant="h5"style={{ fontWeight: 'bold' }} >サービスの使い方</Typography>
           </Grid>
         </Grid>
         <Grid className={`${styles.cardWrapper}`} container >
@@ -53,8 +53,9 @@ const TopDescription = () => {
             <Card
               className={classes.card}
             ><strong>お題に沿って絵を描こう！！</strong></Card>
-            <p className={`${styles.leftDescription}`} >アプリ内のお絵かき機能を準備しています。<br/> 
-                お絵かき機能を活用してお題に沿った絵を描きましょう。<br/>
+            <p className={`${styles.leftDescription}`} >
+                お題に沿って絵を描きましょう！<br/>
+                投稿すれば、誰かいいねしてくれるかも！？<br />
                 お題は定期的に更新追加をしていく予定です！</p>
           </Grid>
           <Grid className={`${styles.item}`} item xs={12} md={6}>
@@ -65,7 +66,7 @@ const TopDescription = () => {
           <Grid className={`${styles.item}`} item xs={12} md={6}> 
             <Card
               className={classes.rightCard}
-            ><strong>Twitterでシェアしよう！！</strong></Card>
+            ><strong>Twitterでシェアしよう！（実装中）</strong></Card>
             <p className={`${styles.rightDescription}`}>描いた絵をTwitterでシェアしましょう！<br/> 
                 ちょっとしたクイズ形式で投稿ができます！<br/>
                 あなたの画才が注目されるチャンスかも！？</p>
@@ -79,9 +80,9 @@ const TopDescription = () => {
             <Card
               className={classes.card}
             ><strong>たくさん「いいね」しよう！！</strong></Card>  
-            <p className={`${styles.leftDescription}`}>いいな、と思った絵には積極的にいいねをしましょう！<br/> 
-              今後月間MVPとして「ベスト・オブ・画伯」賞を贈呈する予定です。<br/>
-              光る画才を見つけたら迷わず「いいね」しましょう！</p>
+            <p className={`${styles.leftDescription}`}>いいな、と思った絵にいいねしましょう！<br/> 
+              いいねの多いユーザーには月間MVP賞を贈呈する予定です。<br/>
+              光る才能を見つけたら迷わず「いいね」しましょう！</p>
           </Grid>
           <Grid className={`${styles.item}`} item xs={12} md={6}>
             <img className={classes.image} src={thirdImage} alt="thirdImage" />
