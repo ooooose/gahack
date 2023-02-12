6e307d8b4a605e3fc61e3bd92667a9cd21d54805
ooooose <yuuki.oose1990@gmail.com>
2023-02-12 13:36:05 +0900

    [Fix]パスワードリセットのバグを解消

--- api/app/controllers/api/v1/auth/passwords_controller.rb
+++ api/app/controllers/api/v1/auth/passwords_controller.rb
@@ -8,16 +8,4 @@ class Api::V1::Auth::PasswordsController < DeviseTokenAuth::PasswordsController
 
     reset_password_failed
   end
-
-  def send_reset_mail_success
-    render json: { message: 'Send Reset Mail successfully' }
-  end
-
-  def reset_password_success
-    render json: { message: 'Password Reset successfully' }
-  end
-
-  def rest_password_failed
-    render json: { message: 'Something went wrong.' }
-  end
 end
