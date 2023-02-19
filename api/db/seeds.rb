fb31c80d67cfb0d1b6ffade93d75f5de340d454b
ooooose <yuuki.oose1990@gmail.com>
2023-02-19 23:18:03 +0900

    [Add]seedデータの投入

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,4 +1,4 @@
 ## 確認用にデータをいくつか用意する。
 
 # Theme.find_or_create_by(title: "アメリカ大陸を見つけたコロンブス")
-AdminUser.create!(email: 'admin@example.com', password: 'adminadmin', password_confirmation: 'adminadmin')
+AdminUser.create!(email: 'admin@example.com', password: 'gahackadmin', password_confirmation: 'gahackadmin')
