c11d6cac56f24d71b86c444f2fa29e7257748e9a
ooooose <yuuki.oose1990@gmail.com>
2023-02-19 23:13:39 +0900

    [Delete]不要なカラムを削除

--- api/db/migrate/20230217133831_add_role_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddRoleToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :role, :integer, null: false, default: 0
-  end
-end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -120,7 +120,6 @@ ActiveRecord::Schema.define(version: 2023_02_17_231441) do
     t.text "tokens"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "role", default: 0, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
