7a72f747d3ebc85a853b42c2980402d72c23a375
ooooose <yuuki.oose1990@gmail.com>
2023-02-21 09:29:30 +0900

    [Fix]リロードするとログアウトするバグを修正

--- api/app/controllers/api/v1/auth/sessions_controller.rb
+++ api/app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::Auth::SessionsController < ApiController
   def index
     if current_api_v1_user
-      render json: { status: 200, current_user: current_api_v1_user }
+      render json: { is_login: true, data: current_api_v1_user }
     else
-      render json: { status: 500, message: "ユーザーが存在しません" }
+      render json: { is_login: false, message: "ユーザーが存在しません" }
     end
   end
 end
