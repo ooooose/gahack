df908da298a092d7982c0ac9dac8d92799eed452
ooooose <yuuki.oose1990@gmail.com>
2023-02-26 16:44:07 +0900

    [Add]cors.rbにURLを１つ追加

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8000', 'https://gahack.netlify.app'
+    origins 'localhost:8000', 'https://gahack.netlify.app', 'https://gahack.net'
 
     resource '*',
       headers: :any,
