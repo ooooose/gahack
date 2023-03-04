8cc4050d52ee5623c863daf4d077147c7636d93c
ooooose <yuuki.oose1990@gmail.com>
2023-03-04 16:43:04 +0900

    [Add]画像アップロード先を変更

--- api/config/initializers/carrierwave.rb
+++ api/config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'gahack-app'
+    config.fog_directory  = 'gahack'
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
