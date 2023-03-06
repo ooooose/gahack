9d8e86c73a3d9ba7cc456577ab9f878824302eab
ooooose <yuuki.oose1990@gmail.com>
2023-03-06 19:14:01 +0900

    [Fix]ホストURLを変更

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_options = { from: ENV['EMAIL_ADDRESS'] }
-  config.action_mailer.default_url_options = { host: 'https://gahack.magia.runteq.jp/api/v1', protocol: "https" }
+  config.action_mailer.default_url_options = { host: 'https://gahack.herokuapp.com', protocol: "https" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
