bc105b335a0c9efc9ffe7c0db4c85466994d2261
ooooose <yuuki.oose1990@gmail.com>
2023-04-01 17:32:59 +0900

    [Fix]テストに基づく修正

--- api/app/models/theme.rb
+++ api/app/models/theme.rb
@@ -1,6 +1,6 @@
 class Theme < ApplicationRecord
   has_many :pictures, -> { order(created_at: :desc) }, dependent: :destroy
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
 
   scope :recent, -> { order(created_at: :desc) }
 end
