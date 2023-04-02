76184096b0f346dde06e6f0d1a57a8caeba4c629
ooooose <yuuki.oose1990@gmail.com>
2023-04-02 22:56:56 +0900

    [Add]コメントに関するテストの記述

--- api/spec/models/comment_spec.rb
+++ api/spec/models/comment_spec.rb
@@ -1,5 +1,44 @@
 require "rails_helper"
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = create(:user)
+    @picture = create(:picture)
+  end
+  
+  it 'ユーザーが絵に対してコメントを入力できること' do
+    comment = Comment.new(
+      body: 'comment',
+      user: @user,
+      picture: @picture
+    )
+    expect(comment).to be_valid
+  end
+
+  it 'ユーザー情報がない場合、コメントが作成できないこと' do
+    comment = Comment.new(
+      body: 'comment',
+      user: nil,
+      picture: @picture
+    )
+    expect(comment).not_to be_valid
+  end
+
+  it '絵の情報がない場合、コメントが作成できないこと' do
+    comment = Comment.new(
+      body: 'comment',
+      user: @user,
+      picture: nil, 
+    )
+    expect(comment).not_to be_valid
+  end
+
+  it 'コメント内容がない場合、コメントの作成ができないこと' do
+    comment = Comment.new(
+      body: nil,
+      user: @user,
+      picture: @picture
+    )
+    expect(comment).not_to be_valid
+  end
 end
