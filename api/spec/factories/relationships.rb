6f43f64ac9ba72ba628f3c85289e968254447298
ooooose <yuuki.oose1990@gmail.com>
2023-04-03 16:04:17 +0900

    [Add]フォロー機能のテスト記述

--- api/spec/factories/relationships.rb
+++ api/spec/factories/relationships.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :relationship do
-    follower_id { 1 }
-    followed_id { 1 }
+    user_id { FactoryBot.create(:user).id }
+    follow_id { FactoryBot.create(:user).id }
   end
 end
