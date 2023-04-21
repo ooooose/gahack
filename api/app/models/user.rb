4c5200efb1ddfa46899b96c05e457b1c06b0ec79
ooooose <yuuki.oose1990@gmail.com>
2023-04-22 06:57:06 +0900

    [Fix]inverse_ofの修正

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   # フォロー、リフォロー
   has_many :relationships, dependent: :destroy
   has_many :followings, through: :relationships, source: :follow
-  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "follow_id", inverse_of: :user, dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "follow_id", inverse_of: :follow, dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :user
 
   scope :without_guests, -> { where.not(name: "ゲストユーザー") }
